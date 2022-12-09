c64ce11a3317c7681ffc6485520fbdc8707dc56b
ojusingb <ojusingb@gmail.com>
2022-12-09 11:05:31 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints without printf or puts
+ * Return: 1
+*/
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-1\n", 58);
+	return (0);
+}
