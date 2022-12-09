f9e2b18ca4b9e79117dae81898d76a32be3b536a
ojusingb <ojusingb@gmail.com>
2022-12-08 17:04:01 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* main - writing a C program using the puts function;
+ 
+ *use of printf is not allowed;
+
+ * return value(0);
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
+
+
