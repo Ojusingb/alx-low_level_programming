4298dc878342bd9f6c27eba9a1a22207ac60e3e1
ojusingb <ojusingb@gmail.com>
2022-12-13 20:33:46 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry
+ * Return: 0
+*/
+
+void print_alphabet(void)
+
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+
+		_putchar(abc);
+
+	_putchar('\n');
+
+}
