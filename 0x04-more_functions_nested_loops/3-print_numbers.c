7f6bbba54af1917aa4d0ba783f90ee8623ef66fb
ojusingb <ojusingb@gmail.com>
2022-12-16 18:32:55 -0800

    3-print_numbers1

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_numbers - prints number 0 to 9
+* print_numbers - print 0123456789
 * print with newline
 * Return: void
 */
@@ -13,7 +13,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c);
+	_putchar(c);
 	}
 	_putchar('\n');
 }
