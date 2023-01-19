2a2f1e0da109818947a132063c6a3968d7b22be2
ojusingb <ojusingb@gmail.com>
2023-01-19 15:50:47 -0800

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
 * print_numbers - a function that prints numbers, followed by a new line
@@ -18,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(printNum, int));
-		if (i < n-1)
+		if (i < n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
